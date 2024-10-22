41fbf68769b347c6a50d719a642061f9379a69bf
Gerardo E. Huesca <Yayo0@LAPTOP-GB7FKC8M>
2024-10-22 12:19:28 -0600

    finish video 9

--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
 using Shopping.Data;
+using Shopping.Data.Entities;
+using Shopping.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,15 +14,44 @@ builder.Services.AddDbContext<DataContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddIdentity<User, IdentityRole>(cfg =>
+{
+    cfg.User.RequireUniqueEmail = true;
+    cfg.Password.RequireDigit = false;
+    cfg.Password.RequiredUniqueChars = 0;
+    cfg.Password.RequireLowercase = false;
+    cfg.Password.RequireNonAlphanumeric = false;
+    cfg.Password.RequireUppercase = false;
+}).AddEntityFrameworkStores<DataContext>();
+
+
+//3 maneras de inyectar
+builder.Services.AddTransient<SeedDb>(); //La voy a usar una vez y lo destruye cuando lo utilzan
+                                         //builder.Services.AddScoped<SeeDb>(); //Es que la inyecta cada que necesita y la destruye cuando se deja de ocupar.
+                                         //builder.Services.AddSingleton<SeeDb>(); //Lo inyecta una vez y no lo destruye.
+
+builder.Services.AddScoped<IUserHelper, UserHelper>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation(); //ACTUALIZA LAS VISTAS SIN TENER QUE DEJAR DE CORRER EL PROGRAMA
 
 var app = builder.Build();
+SeedData();
 
+void SeedData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb?>();
+        service.SeedAsync().Wait();
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    
+
     app.UseHsts();
 }
 
@@ -27,11 +60,16 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+app.UseAuthentication();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+    //}
+//}
 //add-migration
 //add-migration AddIndexToCountry
 //update-database
+
+         
