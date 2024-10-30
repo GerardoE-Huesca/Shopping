cc81493ed771aae187d62f78e97594a8f694e69f
GerardoE-Huesca <Yayohuesca@gmail.com>
2024-10-30 09:56:22 -0600

    Add files via upload

--- Shopping/Shopping/Models/LoginViewModel.cs
+++ Shopping/Shopping/Models/LoginViewModel.cs
@@ -9,13 +9,11 @@ namespace Shopping.Models
         [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
         public string Username { get; set; }
 
-
         [DataType(DataType.Password)]
         [Display(Name = "Contraseña")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [MinLength(6, ErrorMessage = "El campo {0} debe tener al menos {1} carácteres.")]
         public string Password { get; set; }
-
 
         [Display(Name = "Recordarme en este navegador")]
         public bool RememberMe { get; set; }
